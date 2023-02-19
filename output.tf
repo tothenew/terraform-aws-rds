8209852305f2956048d4c9dac8045f118494991d
ayushme001 <ayush.me001@gmail.com>
2023-02-19 21:37:56 +0530

    added more outputs

--- output.tf
+++ output.tf
@@ -5,3 +5,17 @@ output "endpoint" {
 output "identifier" {
   value = var.create_rds == true ? aws_db_instance.rds_db[0].identifier : aws_rds_cluster.aurora_cluster[0].cluster_identifier
 }
+
+output "username" {
+  value = var.create_rds == true ? aws_db_instance.rds_db[0].username : aws_rds_cluster.aurora_cluster[0].username
+}
+
+output "password" {
+  value = var.create_rds == true ? aws_db_instance.rds_db[0].password : aws_rds_cluster.aurora_cluster[0].password
+  sensitive = true
+}
+
+output "port" {
+  value = var.create_rds == true ? aws_db_instance.rds_db[0].port : aws_rds_cluster.aurora_cluster[0].port
+  sensitive = true
+}
