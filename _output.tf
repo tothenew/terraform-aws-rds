4781c4a5b0b55d5eb77f590997ae941f196407ca
Prashant Gupta <ven_pgupta@reflexion.com>
2023-05-24 16:33:42 +0530

    Refactor the Mysql RDS

--- _output.tf
+++ _output.tf
@@ -1,25 +1,3 @@
-#output "endpoint" {
-#  value = var.create_rds == true ? aws_db_instance.rds_db[0].endpoint : aws_rds_cluster.aurora_cluster[0].endpoint
-#}
-#
-#output "identifier" {
-#  value = var.create_rds == true ? aws_db_instance.rds_db[0].identifier : aws_rds_cluster.aurora_cluster[0].cluster_identifier
-#}
-#
-#output "username" {
-#  value = var.create_rds == true ? aws_db_instance.rds_db[0].username : aws_rds_cluster.aurora_cluster[0].master_username
-#}
-#
-#output "password" {
-#  value     = var.create_rds == true ? aws_db_instance.rds_db[0].password : aws_rds_cluster.aurora_cluster[0].master_password
-#  sensitive = true
-#}
-#
-#output "port" {
-#  value     = var.create_rds == true ? aws_db_instance.rds_db[0].port : aws_rds_cluster.aurora_cluster[0].port
-#  sensitive = true
-#}
-
 output "endpoint" {
   value = var.create_aurora == true ? aws_rds_cluster.rds_cluster[0].endpoint : ""
 }
