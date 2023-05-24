73befbdbff2ecadb4cc78f57b032cbe6c8af4f69
Prashant Gupta <ven_pgupta@reflexion.com>
2023-05-24 16:33:30 +0530

    Refactor the Mysql RDS

--- _output.tf
+++ _output.tf
@@ -20,10 +20,22 @@
 #  sensitive = true
 #}
 
+output "endpoint" {
+  value = var.create_aurora == true ? aws_rds_cluster.rds_cluster[0].endpoint : ""
+}
+
+output "identifier" {
+  value = var.create_aurora == true ? aws_rds_cluster.rds_cluster[0].cluster_identifier : ""
+}
+
+output "port" {
+  value     = var.create_aurora == true ? aws_rds_cluster.rds_cluster[0].port : ""
+}
+
 output "username" {
-  value = var.create_aurora == true ? var.master_username : ""
+  value = var.master_username
 }
 
 output "password" {
-  value     = var.create_aurora == true ? var.create_username_password ? random_string.rds_db_password[0].result : var.master_password : ""
+  value     = var.create_username_password ? random_string.rds_db_password[0].result : var.master_password
 }
