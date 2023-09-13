aad4efc1789c3e881244dd5bcc0c121ca2d1ab3d
narenderttn <narender.panwar@tothenew.com>
2023-09-13 15:41:01 +0530

    changes

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_db_instance" "rds_instance" {
   db_name                    = var.database_name == "" ? local.default_database_name : var.database_name
   backup_retention_period    = var.backup_retention_period
   username                   = var.master_username
-  password                   = var.create_username_password ? random_string.rds_db_password[0].result : var.master_password
+  password                   = random_string.rds_db_password.result
   db_subnet_group_name       = var.create_subnet_group ? aws_db_subnet_group.subnet_group[0].name : var.subnet_group_name
   vpc_security_group_ids     = var.create_security_group ? [aws_security_group.security_group[0].id] : var.security_group_ids
   apply_immediately          = var.apply_immediately
