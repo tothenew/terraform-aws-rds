dfbfd4e1efde4991fca49bb5da20b7493751d66d
narenderttn <narender.panwar@tothenew.com>
2023-09-13 14:47:32 +0530

    added third subnet

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 module "mysql_database" {
   source = "git::https://github.com/narenderttn/terraform-aws-rds.git"
 
-  subnet_ids = ["subnet-043d59b3957d49e1d", "subnet-093641ce3f549831e"]
+  subnet_ids = ["subnet-043d59b3957d49e1d", "subnet-093641ce3f549831e", "subnet-0d911d25c86c0a429"]
   vpc_id     = "vpc-0c7ca42512bbbb3df"
 }
