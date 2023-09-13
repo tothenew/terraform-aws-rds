f68fdddf660bbe37605fbef4c8fe7b8588de665b
narenderttn <narender.panwar@tothenew.com>
2023-09-13 14:51:48 +0530

    added AZs

--- _variables.tf
+++ _variables.tf
@@ -136,7 +136,8 @@ variable "engine_version" {
 
 variable "availability_zones" {
   type    = list(string)
-  default = []
+  # default = []
+  default = [ "us-east-1a", "us-east-1b", "us-east-1c" ]
 }
 
 variable "database_name" {
