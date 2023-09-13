0ec4b4a74513c48fe69c17c5f06a2e9953d5d08c
narenderttn <narender.panwar@tothenew.com>
2023-09-13 13:57:24 +0530

    changed to my repo

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "mysql_database" {
-  source = "git::https://github.com/tothenew/terraform-aws-rds.git"
+  source = "git::https://github.com/narenderttn/terraform-aws-rds.git"
 
   subnet_ids = ["subnet-7c16341b", "subnet-b2b59c9c"]
   vpc_id     = "vpc-855826ff"
