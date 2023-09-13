a21e6a0f064936e446acdfa6ebcea51c34294eb1
narenderttn <narender.panwar@tothenew.com>
2023-09-13 13:59:09 +0530

    changed vpc and subnets

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 module "mysql_database" {
   source = "git::https://github.com/narenderttn/terraform-aws-rds.git"
 
-  subnet_ids = ["subnet-7c16341b", "subnet-b2b59c9c"]
-  vpc_id     = "vpc-855826ff"
+  subnet_ids = ["subnet-043d59b3957d49e1d", "subnet-093641ce3f549831e"]
+  vpc_id     = "vpc-0c7ca42512bbbb3df"
 }
