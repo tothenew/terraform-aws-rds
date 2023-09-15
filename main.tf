9c092c942ce2ab43d482656c3568eb74ceb93b47
narenderttn <narender.panwar@tothenew.com>
2023-09-15 11:04:03 +0530

    changes

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_rds_cluster" "rds_cluster" {
   database_name                       = var.database_name == "" ? local.default_database_name : var.database_name
   allocated_storage                   = 256
   db_cluster_instance_class           = "db.r6g.large"
-  # iops                                = 2500
+  iops                                = 2500
   storage_type                        = "io1"
   master_username                     = var.master_username
   master_password                     = var.create_username_password ? random_string.rds_db_password[0].result : var.master_password
