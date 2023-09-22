cd7efbb9de7ea71ce15d6996a96d7b4ebfe6c353
narenderttn <narender.panwar@tothenew.com>
2023-09-22 16:10:14 +0530

    testing

--- _variables.tf
+++ _variables.tf
@@ -7,7 +7,8 @@ variable "name" {
 variable "create_aurora" {
   type        = bool
   description = "If you want to create Aurora MySQL or PostgreSQL enable this check"
-  default     = true
+  # default     = true
+  default     = false
 }
 
 variable "database_module" {
@@ -116,19 +117,22 @@ variable "subnet_ids" {
 variable "parameter_family" {
   type        = string
   description = "The family of the DB parameter group for mysql(aurora-mysql8.0) or postgresql(aurora-postgresql15)"
-  default = "aurora-mysql8.0"
+  # default = "aurora-mysql8.0"
+  default = "mysql8.0"
 }
 
 variable "engine" {
   type        = string
   description = "The name of the database engine to be used for this DB cluster for mysql(aurora-mysql, mysql) or postgresql(aurora-postgresql, postgresql)"
-  default     = "aurora-mysql"
+  # default     = "aurora-mysql"
+  default     = "mysql"
 }
 
 variable "engine_version" {
   type        = string
   description = "The name of the database engine to be used for this DB cluster for mysql(8.0.mysql_aurora.3.02.0, 8.0) or postgresql(13.6)"
-  default     = "8.0.mysql_aurora.3.02.0"
+  # default     = "8.0.mysql_aurora.3.02.0"
+  default     = "8.0"
 }
 
 variable "multi_az" {
