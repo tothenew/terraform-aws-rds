6ce90a48f85829951f0de5c20819a7c1c3a04edd
narenderttn <narender.panwar@tothenew.com>
2023-09-22 13:57:17 +0530

    minor changes

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 module "mysql_database" {
   source = "git::https://github.com/tothenew/terraform-aws-rds.git"
 
-  subnet_ids = ["subnet-043d59b3957d4", "subnet-093641ce3f549", "subnet-0d911d25c86c0"]
+  subnet_ids = ["subnet-043d59b3957d4", "subnet-093641ce3f549"]
   vpc_id     = "vpc-0c7ca42512bb"
 }
